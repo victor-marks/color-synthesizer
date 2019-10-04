cf8debae5abe4f4aa5db4e45d538288efa2fed4c
Victor Marks <vjmarks@gmail.com>
2019-10-03 23:30:50 -0700

    added and styled color slider

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -1,18 +1,18 @@
 import chroma from 'chroma-js';
 const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
-function generatePalette(starterPallete) {
+function generatePalette(starterPalette) {
   let newPalette = {
-    paletteName: starterPallete.paletteName,
-    id: starterPallete.id,
-    emoji: starterPallete.emoji,
+    paletteName: starterPalette.paletteName,
+    id: starterPalette.id,
+    emoji: starterPalette.emoji,
     colors: {}
   };
   for (let level of levels) {
     newPalette.colors[level] = [];
   }
-  for (let color of starterPallete.colors) {
-    let scale = getScale(color.color).reverse();
+  for (let color of starterPalette.colors) {
+    let scale = getScale(color.color, 10).reverse();
     for (let i in scale) {
       newPalette.colors[levels[i]].push({
         name: `${color.name} ${levels[i]}`,
@@ -21,14 +21,13 @@ function generatePalette(starterPallete) {
         rgb: chroma(scale[i]).css(),
         rgba: chroma(scale[i])
           .css()
-          .replace('rgb', 'rgba')
+          .replace('rgba', 'rgba')
           .replace(')', ',1.0)')
       });
     }
   }
   return newPalette;
 }
-
 function getRange(hexColor) {
   const end = '#fff';
   return [
